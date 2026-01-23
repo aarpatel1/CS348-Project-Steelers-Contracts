3d94396f868bfc52607f63bc9fab57213d9255da
aarpatel1 <pate2208@purdue.edu>
2026-01-23 00:33:53 -0500

    initial push

---
+++ steelers-contracts/src/main/java/edu/purdue/cs348/steelers_contracts/SteelersContractsApplication.java
@@ -0,0 +1,13 @@
+package edu.purdue.cs348.steelers_contracts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteelersContractsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteelersContractsApplication.class, args);
+	}
+
+}
